dd6e042db84a7d7b9540c8d83d1787ddafbf61c4
Rndmi <SireOfAll79@yandex.ru>
2023-04-17 13:22:25 +0300

    HW 17.07 initial commit

---
+++ src/Course2/HW3/Bicycle.java
@@ -0,0 +1,19 @@
+package Course2.HW3;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+}
