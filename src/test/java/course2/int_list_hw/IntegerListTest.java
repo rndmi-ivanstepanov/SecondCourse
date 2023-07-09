d24ca03d62c55ed26d20b6db3c41bf3b04302cb7
Ivan Stepanov <SireOfAll79@yandex.ru>
2023-07-09 14:41:53 +0300

    integer list and sorts comparison

---
+++ src/test/java/course2/int_list_hw/IntegerListTest.java
@@ -0,0 +1,101 @@
+package course2.int_list_hw;
+
+import course2.string_list_hw.exceptions.ElementNotFoundException;
+import course2.string_list_hw.exceptions.IdxOutOfBoundsException;
+import course2.string_list_hw.exceptions.NullItemException;
+import org.junit.jupiter.api.Test;
+
+import static course2.int_list_hw.constants.Constants.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegerListTest {
+    IntList out = new IntegerList();
+
+    private void fill(IntList arr, int upTo) {
+        for (int i = 0; i < upTo; i++) {
+            arr.add(NUM);
+        }
+    }
+
+    @Test
+    void testAdd() {
+        fill(out, 5);
+        assertEquals(out.get(4), NUM);
+
+        out.add(0, NUM2);
+        assertEquals(0, out.indexOf(NUM2));
+    }
+
+    @Test
+    void testSet() {
+        fill(out, 3);
+        out.set(2, NUM2);
+        assertEquals(2, out.indexOf(NUM2));
+    }
+
+    @Test
+    void testRemove() {
+        fill(out, 1);
+        out.remove(NUM);
+        assertTrue(out.isEmpty());
+        assertThrows(NullItemException.class, () -> out.remove(NULL));
+        assertThrows(ElementNotFoundException.class, () -> out.remove(NUM));
+    }
+
+    @Test
+    void testContains() {
+        fill(out, 5);
+        assertTrue(out.contains(NUM));
+        assertFalse(out.contains(NUM2));
+    }
+
+    @Test
+    void testIndexOf() {
+        fill(out, 100);
+        assertEquals(-1, out.lastIndexOf(NUM2));
+        assertEquals(99, out.lastIndexOf(NUM));
+        assertEquals(0, out.indexOf(NUM));
+    }
+
+    @Test
+    void get_shouldThrowIfIdxIsBiggerThanLength() {
+        fill(out, 10);
+        assertThrows(IdxOutOfBoundsException.class, () -> out.get(10));
+    }
+
+    @Test
+    void testEquals() {
+        fill(out, 2);
+        IntList clone = new IntegerList();
+        fill(clone, 2);
+        assertEquals(out.size(), clone.size());
+        assertTrue(out.equals(clone));
+        clone.add(NUM2);
+        assertFalse(out.equals(clone));
+        fill(out, 100);
+        assertFalse(out.equals(clone));
+    }
+
+    @Test
+    void size_shouldReturnZeroIfArrIsEmpty() {
+        assertEquals(0, out.size());
+    }
+
+    @Test
+    void testIsEmpty() {
+        assertTrue(out.isEmpty());
+        fill(out, 10);
+        assertFalse(out.isEmpty());
+    }
+
+    @Test
+    void clear_shouldClearTheArrayAndNullifyTheSize() {
+        fill(out, 100);
+        assertFalse(out.isEmpty());
+        assertNotEquals(0, out.size());
+
+        out.clear();
+        assertTrue(out.isEmpty());
+        assertEquals(0, out.size());
+    }
+}
